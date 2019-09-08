bd0464b603434bba04f752e0e09ff3a884bae24b
tomoro <n.tomoro13@gmail.com>
2019-09-08 22:38:49 +0900

    パスワード暗号化テスト

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,75 @@
 import express from 'express'
 import 'reflect-metadata';
+import bcrypt from "bcrypt"
 import { User } from './entities/User'
 import { getConnectionOptions, createConnection, BaseEntity } from 'typeorm'
 
 createConnection().then(async (connection) => {
-  const app = express();
+  const app = express()
+
+  // 接続するたびuserを追加
   app.get('/', async (req, res) => {
-    const user = new User();
+    const user = new User()
+
+    // パスワード暗号化
+    const hasedPassword = await bcrypt.hash('0000', 10)
     user.id = 6
-    user.login = 'adimn' + Math.random().toString();
-    user.password = '0000';
-    await User.save(user);
-    res.send(user);
-  });
+    // Math.random()についてはランダムにしたいだけ
+    user.login = 'adimn' + Math.random().toString()
+    user.password = hasedPassword
+    await User.save(user)
+    res.send(user.id.toString())
+  })
+
+  // user一覧を閲覧
   app.get('/read', async (req, res) => {
-    // const users = await User.find()
-    const users = await User.find({});
+    const users = await User.find({})
 
-    // if (users) await users.remove();
     if (users) {
-      res.send(users);
+      res.send(users)
     }
     else {
       res.send("no such user")
     }
-  });
+  })
+
+  // 接続するたびひとつuserを削除
   app.get('/delete', async (req, res) => {
-    // const users = await User.find()
-    const users = await User.findOne({});
+    const users = await User.findOne({})
 
-    if (users) await users.remove();
+    if (users) await users.remove()
     if (users) {
-      res.send(users);
+      res.send(users)
     }
     else {
       res.send("no such user")
     }
-  });
-  app.listen(3000, () => console.log('Example app listening on port 3000!'));
-}).catch((error) => console.log(error));
+  })
+
+
+  app.get('/compare', async (req, res) => {
+    const users = await User.find()
+
+    if (users) {
+      // 正しく暗号化されてるかテストしたい
+      const getResults = async () => {
+        let results: boolean[] = []
+        for (let i = 0; i < users.length; i++) {
+          const result = await bcrypt.compare("0000", users[i].password)
+          results.push(result)
+        }
+        return results
+      }
+
+      res.send(await getResults())
+    }
+    else {
+      res.send("no such user")
+    }
+  })
+
+
+  app.listen(4000, () => console.log('Example app listening on port 4000!'))
+
+
+}).catch((error) => console.log(error))
